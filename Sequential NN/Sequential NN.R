2882883af5a5f35929a7c66cd7b22e1f49330f79
Sidi Wang <sidiwang@umich.edu>
2019-12-20 21:21:36 -0500

    fitted Sequential NN via Keras Package in R

---
+++ Sequential NN/Sequential NN.R
@@ -0,0 +1,52 @@
+library(keras)
+library(tidyverse)
+library(tensorflow)
+
+load(file="NNdata.rda")
+
+# specify model features
+max_features <- 35000
+maxlen <- 6122                                                                 
+batch_size <-15
+embedding_dims <- 50
+filters <- 32
+kernel_size <- c(3)
+hidden_dims <- 300
+epochs <- 7
+
+# build sequential model
+model <- keras_model_sequential() %>% 
+
+  layer_dense(hidden_dims) %>%
+  layer_dropout(0.3) %>%
+  layer_activation("relu") %>%
+  layer_dense(units = 48, activation = 'softmax') %>% compile(
+    loss = "categorical_crossentropy",
+    optimizer = "adam",
+    metrics = "accuracy"
+  )
+
+x <- as.matrix(M_dgt) 
+y <- disease_factor
+
+train_x <- x[1:100000,]
+train_y <- y[1:100000,]
+
+test_x <- x[100001:153956,]
+test_y <- y[100001:153956,]
+
+history <- model %>%
+  fit(
+    train_x,
+    train_y,
+    batch_size = batch_size,
+    epochs = epochs,
+    # validation_split = 0.3
+  )
+
+result <- model %>% evaluate(test_x, test_y)
+
+# load training outcome
+load(file="NNmodel.rda")
+result # testing accuracy = 0.8323264
+history # trianing accuracy = 0.8937
